ee7fef132e4dd0a20b7f88d3a11c250d9a0ce4f4
Tong Zhang <zhangt_1996@163.com>
2019-07-11 11:05:17 +0800

    fourth commit

--- cpp-4/cpp4/cpp4/cpp4.cpp
+++ cpp-4/cpp4/cpp4/cpp4.cpp
@@ -1,10 +1,43 @@
 #include<iostream>
 #include<cstring>
 #include<string>
+struct inflatable   // structure declaration
+{
+	char name[20];
+	float volume;
+	double price;
+};
 int main()
 {
 	using namespace std;
 
+
+
+	inflatable guest =
+	{
+		"Glorious Gloria",  // name value
+		1.88,               // volume value
+		29.99               // price value
+	};  
+	inflatable pal =
+	{
+		"Audacious Arthur",
+		3.12,
+		32.99
+	};  // NOTE: some implementations require using static inflatable guest =
+	cout << "Expand your guest list with " << guest.name;
+	cout << " and " << pal.name << "!\n";
+	cout << "You can have both for $";
+	cout << guest.price + pal.price << "!\n";
+	inflatable choice = pal;
+	cout << choice.name << " " << choice.volume << endl;
+	inflatable guests[2] =
+	{
+		{"auda",123,3.66},
+		{"qwwre",666,0.15}
+	};
+	cout << guests[0].name << endl;
+	
 	char charr[20];
 	string str;
 	cin >> str;
