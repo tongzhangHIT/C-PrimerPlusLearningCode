211de7842a7fc6cc91961e64ca5aa7ca38377aac
Tong Zhang <zhangt_1996@163.com>
2019-07-13 15:42:54 +0800

    fourth commit

--- cpp-4/cpp4/cpp4/cpp4.cpp
+++ cpp-4/cpp4/cpp4/cpp4.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<cstring>
 #include<string>
+#include<vector>
+#include<array>
 char* getname(void);
 struct inflatable   // structure declaration
 {
@@ -8,10 +10,55 @@ struct inflatable   // structure declaration
 	float volume;
 	double price;
 };
+struct antarctica_years_end
+{
+	int year;
+};
 int main()
 {
 	using namespace std;
 	
+	// C, original C++
+	double a1[4] = { 1.2, 2.4, 3.6, 4.8 };
+	// C++98 STL
+	vector<double> a2(4);   // create vector with 4 elements
+	// no simple way to initialize in C98
+	a2[0] = 1.0 / 3.0;
+	a2[1] = 1.0 / 5.0;
+	a2[2] = 1.0 / 7.0;
+	a2[3] = 1.0 / 9.0;
+	// C++0x -- create and initialize array object
+	array<double, 4> a3 = { 3.14, 2.72, 1.62, 1.41 };
+	array<double, 4> a4;
+	a4 = a3;     // valid for array objects of same size
+	// use array notation
+	cout << "a1[2]: " << a1[2] << " at " << &a1[2] << endl;
+	cout << "a2[2]: " << a2[2] << " at " << &a2[2] << endl;
+	cout << "a3[2]: " << a3[2] << " at " << &a3[2] << endl;
+	cout << "a4[2]: " << a4[2] << " at " << &a4[2] << endl;
+	// misdeed
+	a1[-6] = 20.2;
+	cout << "a1[-6]: " << a1[-6] << " at " << &a1[-6] << endl;
+	cout << "a3[2]: " << a3[2] << " at " << &a3[2] << endl;
+	cout << "a4[2]: " << a4[2] << " at " << &a4[2] << endl;
+	a2.at(1) = 2.3;
+	cout << "a2[1]: " << a2[1] << " at " << &a2[1] << endl;
+
+	antarctica_years_end s01, s02, s03;
+	s01.year = 1998;
+	antarctica_years_end* pa = &s02;
+	pa->year = 1999;
+	antarctica_years_end trio[3]; // array of 3 structures
+	trio[0].year = 2003;
+	cout << trio->year << endl;
+	const antarctica_years_end* arp[3] = { &s01, &s02, &s03 };
+	cout << arp[1]->year << endl;
+	const antarctica_years_end** ppa = arp;
+	auto ppb = arp; // C++0x automatic type deduction
+	// or else use const antarctica_years_end ** ppb = arp; 
+	cout << (*ppa)->year << endl;
+	cout << (*(ppb + 1))->year << endl;
+
 	char* name;
 	name = getname();
 	cout << name << " at " << (int*)name << endl;
