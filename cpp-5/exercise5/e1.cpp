1c0ba3fb7f605e6c2f51a7858220a4ae5c688995
Tong Zhang <zhangt_1996@163.com>
2019-07-15 17:53:56 +0800

    vs commit

--- cpp-5/exercise5/e1.cpp
+++ cpp-5/exercise5/e1.cpp
@@ -2,6 +2,13 @@
 int main()
 {
 	using namespace std;
-
+	int a, b, sum=0;
+	cout << "a: ";
+	cin >> a;
+	cout << "b: ";
+	cin >> b;
+	for (int i = a; i <= b;i++)
+		sum += i;
+	cout << "sum= " << sum << endl;
 	return 0;
 }
